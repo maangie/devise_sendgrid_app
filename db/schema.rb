57b15349d4fa09fae37928e8138322bd71829f0e
maangie <maangie906@gmail.com>
2019-03-25 17:11:33 +0900

    confirmable 追加

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2019_03_25_080323) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
