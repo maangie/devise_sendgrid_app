b3685fa84b482ad48e520e20a7d98e209336970a
NAGATA Minoru <nagata.minoru8823@gmail.com>
2021-05-21 19:09:23 +0900

    コントローラ・テスト修正

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # https://github.com/heartcombo/devise#controller-tests
+  include Devise::Test::IntegrationHelpers
+
+  test '新規登録にアクセスできる' do
+    get new_user_registration_url
+    assert_response :success
+  end
 end
